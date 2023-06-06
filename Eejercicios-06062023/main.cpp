b8eb9eee6c820b663f5d3d88e27c390489b6c7d3
Santiago Molpeceres Díaz <santiagomolpeceres@hotmail.com>
2023-06-06 13:10:55 +0200

    ok

---
+++ Eejercicios-06062023/main.cpp
@@ -0,0 +1,102 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct Letras{
+    char letra;
+    int repeticiones;
+};
+
+int LetraUnica(const string &texto);
+void Ejercicio1(const string &texto);
+void Ejercicio2(const string &texto);
+bool search(const vector<Letras> &letras,char letra_b);
+void print(const vector<Letras> listado);
+
+int main()
+{
+    int opcion{1};
+    do{
+        cout<<"Texto a anlizar: ";
+        string texto;
+        cin>>texto;
+        // Ejercicio 1
+        Ejercicio1(texto);
+        //Ejercicio 2
+        Ejercicio2(texto);
+
+        cout<<"\n Quieres volver a hacerlo? 1 SI 0 No \n Eleccion: ";
+        cin>>opcion;
+        system("cls");
+    }while(opcion!=0);
+    cout<<"\n Chaooo";
+    return 0;
+}
+
+void Ejercicio1(const string &texto){
+    int letra= LetraUnica(texto);
+    cout <<"\n letra:"<<letra;
+    if(letra != -1){
+        cout<<"\nLa primera letra que no se repite es: "<<texto[letra]<<"\n";
+    }else{
+        cout<<"\n Todas las letras se repiten ";
+    };
+}
+
+
+void Ejercicio2(const string &texto){
+    vector<Letras> Letras_del_texto;
+    for(int i=0;i<texto.size();i++){
+        int contador =0;
+        bool registrado = search(Letras_del_texto,texto.at(i));
+        if(registrado==0){
+            for(int j=0 ; j<texto.size();j++){
+                if(texto.at(i)==texto.at(j)){
+                    contador++;
+                }
+            }
+
+            Letras letra{
+                texto.at(i),
+                        contador
+            };
+            Letras_del_texto.push_back(letra);
+        }
+    }
+    print(Letras_del_texto);
+}
+
+int LetraUnica(const string &texto){
+    for(int i=0;i<texto.size();i++){
+        int contador =0;
+        for(int j=0;j<texto.size();j++){
+            if(texto.at(i)==texto.at(j)){
+                contador+=1;
+            }
+        }
+        if(contador==1){
+            return i;
+        }
+    }
+    return -1;
+}
+
+
+bool search(const vector<Letras> &letras,char letra_b){
+    for(const Letras &letra: letras){
+        if(letra.letra == letra_b){
+            return 1;
+        }
+    }
+    return 0;
+}
+
+void print(const vector<Letras> listado){
+    cout<<"\n Letra - Contaidad \n";
+    for(const Letras &letra:listado){
+        cout<<"\n  "<<letra.letra<<" - "<<letra.repeticiones;
+    }
+
+    cout <<"\n";
+}
