45380a1338801ed92055af590b9101f39a52a840
Carmen Sánchez <54986038+carmenschez5@users.noreply.github.com>
2023-06-12 23:58:55 +0200

    Update FIBONACCI - RECURSIVO TOTAL.cpp

--- Ejercicios-09-06-2023/FIBONACCI - RECURSIVO TOTAL.cpp
+++ Ejercicios-09-06-2023/FIBONACCI - RECURSIVO TOTAL.cpp
@@ -6,65 +6,67 @@ using namespace std;
 
 //ejercicio 1
 int menu() {
-    int respuesta;
-    do {
-        cout << "\n";
-        cout << " -------- FIBONACCI -------- " << "\n";
-        cout << "1. Serie Fibonacci: " << "\n";
-        cout << "0. Salir. " << "\n";
-        cout << "Ingrese una opcion: ";
-        cin >> respuesta;
+	int respuesta;
+	do {
+		cout << "\n";
+		cout << " -------- FIBONACCI -------- " << "\n";
+		cout << "1. Serie Fibonacci: " << "\n";
+		cout << "0. Salir. " << "\n";
+		cout << "Ingrese una opcion: ";
+		cin >> respuesta;
 
-        if (respuesta != 1 && respuesta != 0)
-            cout << "\n\n\t La opcion no es valida. Por favor, vuelva a seleccionar otra opcion. ";
-    } while (respuesta != 1 && respuesta != 0);
+		if (respuesta != 1 && respuesta != 0)
+			cout << "\n\n\t La opcion no es valida. Por favor, vuelva a seleccionar otra opcion. ";
+	} while (respuesta != 1 && respuesta != 0);
 
-    return respuesta;
+	return respuesta;
 }
 
 //Se debe realizar la función de Fibonacci de forma recursiva
 int fibonacci(int N) {
-    if (N <= 1) {
-        return N;
-    }
-    return fibonacci(N - 1) + fibonacci(N - 2);
+	if (N <= 1) {
+		return N;
+	}
+	return fibonacci(N - 1) + fibonacci(N - 2);
 }
 
-void mostrarFibonacci(int n) {
-    if (n <= 0) {
-        cout << "El numero ingresado debe ser mayor o igual a cero." << "\n";
-        return;
-    }
+void mostrarFibonacciRecursivo(int n, int i) {
+	//'n' es el numero FINAL 
+	if (n <= 0) {
+		cout << "El numero ingresado debe ser mayor o igual a 0. ";
+		return;
+	}
 
-    for (int i{ 0 }; i <= n; i++) {
-        cout << fibonacci(i) << " ";
-    }
-    cout << "\n";
+	//'i' es el numero INICIAL
+	if (i <= n) {
+		cout << fibonacci(i) << " ";
+		mostrarFibonacciRecursivo(n, i + 1);
+	}
 }
 
 int main() {
 
-    int opcion = menu();
+	int opcion = menu();
 
-    while (opcion != 0) {
-        int n; //el numero de elementos que va a imprimir
-        cout << " Ingrese un numero: ";
-        cin >> n;
-        switch (opcion) {
-        case 1:
-            mostrarFibonacci(n);
-            break;
-        default:
-            cout << "Opcion invalida. Intente nuevamente." << "\n";
-            return -1;
-        }
+	while (opcion != 0) {
+		int n; //el numero de elementos que va a imprimir
+		cout << " Ingrese un numero: ";
+		cin >> n;
+		switch (opcion) {
+		case 1:
+			mostrarFibonacciRecursivo(n - 1, 0);
+			break;
+		default:
+			cout << "Opcion invalida. Intente nuevamente." << "\n";
+			return -1;
+		}
 
-        opcion = menu();
-    }
+		opcion = menu();
+	}
 
-    system("cls");
+	system("cls");
 
-    cout << "saliendo...";
+	cout << "saliendo...";
 
-    return 0;
+	return 0;
 }
