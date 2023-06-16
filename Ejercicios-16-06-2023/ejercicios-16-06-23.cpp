3d7a8c9df3452543c417ecc833cf89423b4ea307
Carmen Sánchez <54986038+carmenschez5@users.noreply.github.com>
2023-06-16 20:20:14 +0200

    Update ejercicios-16-06-23.cpp

--- Ejercicios-16-06-2023/ejercicios-16-06-23.cpp
+++ Ejercicios-16-06-2023/ejercicios-16-06-23.cpp
@@ -4,29 +4,42 @@
 
 using namespace std;
 
-vector<string> encontrarCadena(const string &cadena, const string &subcadena) { 
-    vector<string> palabrasComunes;
-    string palabra_buscar;
+//CARMEN SÁNCHEZ HERNÁNDEZ 
 
-    for (auto elem: palabra_buscar) {
-        cout <<"\n palabra: " << elem;
+//ejercicio2
+vector<char> encontrarLetra(const string &texto1, const string &texto2) {
+    vector<char> letraComun;
+    char letra_a_buscar{};
+    bool letraExistente = false;
+
+    for (char letra1 : texto2) { 
+        for (char letra2 : texto1) {
+            if (letra1 == letra2) {
+                letraComun.push_back(letra1);
+            }
+            if (letra_a_buscar == letra1) { 
+                letraExistente = true; 
+            }
+        }
     }
 
-    return palabrasComunes;
-
+    return letraComun;
 }
 
 int main() {
 
-    string texto{"pepe toma un te \n"};
-    string subcadena{"el toma te \n"};
+    string texto{"\n una puerta \n"};
+    string subcadena{"\n un perro come \n"};
 
     cout << texto; 
-    cout << subcadena; 
+    cout << subcadena;
 
-    vector<string> encontrado = encontrarCadena(texto, subcadena);
+    vector<char> encontrado = encontrarLetra(texto, subcadena);
+
+    for (auto final : encontrado) {
+        cout << final << "   "; 
+    }
 
     return 0;
     
 }
-
